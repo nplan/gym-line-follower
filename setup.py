f4b422dd25fe16ceddac2bdda3aca241b2782bdd
Nejc Planinšek <>
2020-01-28 20:42:15 +0100

    bugfixes

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
 
 setup(name='gym_line_follower',
-      version='0.1.0',
-      install_requires=['gym', 'pybullet', 'opencv-python', 'shapely', 'numpy'],
+      version='0.1.1',
+      install_requires=['gym==0.10.8',
+                        'pybullet', 'opencv-python', 'shapely', 'numpy'],
       author="Nejc Planinsek",
       author_email="planinseknejc@gmail.com",
       description="Line follower simulator environment.",
